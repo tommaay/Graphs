cf9151dc7f596cbbc3e4c5498579f8d3cd3e192e
ToMMaaY <thuynh629@gmail.com>
2019-04-08 12:24:42 -0700

    implemented a Stack class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -20,6 +20,23 @@ class Queue():
         return (len(self.queue))
 
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return (len(self.stack))
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
